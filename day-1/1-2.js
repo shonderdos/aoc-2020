5ef68e0d0fb102b52571f7afefa15bb834f97b23
Serge Honderdos <serge.honderdos@sytac.io>
2020-12-15 11:07:57 +0100

    day-1

---
+++ day-1/1-2.js
@@ -0,0 +1,32 @@
+const obj = require('./data.json');
+
+const indexArr = [0, 1, 2];
+const isCorrectCombination = (arr, indexArr, expectedResult) => {
+    return indexArr
+        .map(index => arr[index])
+        .reduce((acc, curr) => acc + curr) === expectedResult;
+}
+
+const getResult = (arr, indexArr) => {
+    return indexArr
+        .map(index => arr[index])
+        .reduce((acc, curr) => acc * curr);
+}
+
+const increaseIndexArr = (indexArr, maxIndex) => {
+    let lastIndex = indexArr.pop();
+    lastIndex++;
+
+    if (lastIndex > maxIndex) {
+        lastIndex = [...indexArr].reverse().shift() + 1;
+        increaseIndexArr(indexArr, maxIndex);
+    }
+
+    indexArr.push(lastIndex);
+}
+
+while (!isCorrectCombination(obj.data, indexArr, 2020)) {
+    increaseIndexArr(indexArr, obj.data.length);
+}
+
+console.log(getResult(obj.data, indexArr));
