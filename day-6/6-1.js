ac41ae67869c2055746aebc449c41a2bfca7f466
Serge Honderdos <serge.honderdos@sytac.io>
2022-11-25 08:59:28 +0100

    Update 6-1.js

--- day-6/6-1.js
+++ day-6/6-1.js
@@ -4,16 +4,6 @@ const rawData = fs.readFileSync('./data.txt', 'utf8');
 const rawDataAsArray = rawData
     .split(/\n\n/)
     .map(x => x.replace(/\n/g, ""))
-    .map(string => [...string].reduce((result, char) => {
-        if(!result.includes(char)){
-            result.push(char)
-        }
-
-        return result
-    }, []))
-    .map(string => string.length)
-    .reduce((count, number) => {
-        return count + number
-    }, 0)
+    .reduce((sum, string) => sum + new Set(string).size, 0);
 
 console.log(rawDataAsArray)
